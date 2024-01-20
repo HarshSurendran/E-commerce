8f3a97d22043facc2808cf92bccc002811684329
Harsh Surendran <harshsurendran@gmail.com>
2024-01-20 11:19:34 +0530

    Utils updated

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+
+
+
+
+
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
-
 const connectDB = require("./db/index.js")
+const app = require('./app.js')
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 4000, ()=>{
+        console.log(`Server started at http://localhost:${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("Mongo connection failed", error);
+})
