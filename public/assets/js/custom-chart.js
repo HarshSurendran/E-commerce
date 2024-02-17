04f9630c47b39bd7ddd31d765a33d65d7650b56a
Harsh Surendran <harshsurendran@gmail.com>
2024-02-17 12:11:18 +0530

    some progress in dynamic chart for admin dashboard

--- public/assets/js/custom-chart.js
+++ public/assets/js/custom-chart.js
@@ -2,53 +2,143 @@
     "use strict";
 
     /*Sale statistics Chart*/
+    // if ($('#myChart').length) {
+    //     var ctx = document.getElementById('myChart').getContext('2d');
+    //     var chart = new Chart(ctx, {
+    //         // The type of chart we want to create
+    //         type: 'line',
+            
+    //         // The data for our dataset
+    //         data: {
+    //             labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    //             datasets: [{
+    //                     label: 'Sales',
+    //                     tension: 0.3,
+    //                     fill: true,
+    //                     backgroundColor: 'rgba(44, 120, 220, 0.2)',
+    //                     borderColor: 'rgba(44, 120, 220)',
+    //                     data: [18, 17, 4, 3, 2, 20, 25, 31, 25, 22, 20, 9]
+    //                 },
+    //                 {
+    //                     label: 'Visitors',
+    //                     tension: 0.3,
+    //                     fill: true,
+    //                     backgroundColor: 'rgba(4, 209, 130, 0.2)',
+    //                     borderColor: 'rgb(4, 209, 130)',
+    //                     data: [40, 20, 17, 9, 23, 35, 39, 30, 34, 25, 27, 17]
+    //                 },
+    //                 {
+    //                     label: 'Products',
+    //                     tension: 0.3,
+    //                     fill: true,
+    //                     backgroundColor: 'rgba(380, 200, 230, 0.2)',
+    //                     borderColor: 'rgb(380, 200, 230)',
+    //                     data: [30, 10, 27, 19, 33, 15, 19, 20, 24, 15, 37, 6]
+    //                 }
+
+    //             ]
+    //         },
+    //         options: {
+    //             plugins: {
+    //             legend: {
+    //                 labels: {
+    //                 usePointStyle: true,
+    //                 },
+    //             }
+    //             }
+    //         }
+    //     });
+    // } //End if
+
+    /*Sale statistics Chart*/
+    function fetchDataAndUpdateChart(filter,time) {
+        // Make AJAX request to fetch data from backend
+        $.ajax({
+            url: '/api/v1/admin/salesdata',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(
+                {
+                    filter,
+                    time
+                }
+            ),
+            success: function(response) {
+                console.log("This is response",response);
+                // Update chart with new data
+                chart.data.labels = response.labels;
+                chart.data.datasets[0].data = response.salesData;
+                chart.data.datasets[1].data = response.revenueData;
+                chart.data.datasets[2].data = response.productsData;
+    
+                // Update chart
+                chart.update();
+            },
+            error: function(xhr, status, error) {
+                console.error('Error fetching data from backend:', error);
+            }
+        });
+    }
+    
+    // Check if chart element exists
     if ($('#myChart').length) {
         var ctx = document.getElementById('myChart').getContext('2d');
         var chart = new Chart(ctx, {
-            // The type of chart we want to create
-            type: 'line',
-            
-            // The data for our dataset
-            data: {
-                labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-                datasets: [{
-                        label: 'Sales',
-                        tension: 0.3,
-                        fill: true,
-                        backgroundColor: 'rgba(44, 120, 220, 0.2)',
-                        borderColor: 'rgba(44, 120, 220)',
-                        data: [18, 17, 4, 3, 2, 20, 25, 31, 25, 22, 20, 9]
+                    // The type of chart we want to create
+                    type: 'line',
+                    
+                    // The data for our dataset
+                    data: {
+                        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                        datasets: [{
+                                label: 'Sales',
+                                tension: 0.3,
+                                fill: true,
+                                backgroundColor: 'rgba(44, 120, 220, 0.2)',
+                                borderColor: 'rgba(44, 120, 220)',
+                                data: [18, 17, 4, 3, 2, 20, 25, 31, 25, 22, 20, 9]
+                            },
+                            {
+                                label: 'Revenue',
+                                tension: 0.3,
+                                fill: true,
+                                backgroundColor: 'rgba(4, 209, 130, 0.2)',
+                                borderColor: 'rgb(4, 209, 130)',
+                                data: [40, 20, 17, 9, 23, 35, 39, 30, 34, 25, 27, 17]
+                            },
+                            {
+                                label: 'Products',
+                                tension: 0.3,
+                                fill: true,
+                                backgroundColor: 'rgba(380, 200, 230, 0.2)',
+                                borderColor: 'rgb(380, 200, 230)',
+                                data: [30, 10, 27, 19, 33, 15, 19, 20, 24, 15, 37, 6]
+                            }
+        
+                        ]
                     },
-                    {
-                        label: 'Visitors',
-                        tension: 0.3,
-                        fill: true,
-                        backgroundColor: 'rgba(4, 209, 130, 0.2)',
-                        borderColor: 'rgb(4, 209, 130)',
-                        data: [40, 20, 17, 9, 23, 35, 39, 30, 34, 25, 27, 17]
-                    },
-                    {
-                        label: 'Products',
-                        tension: 0.3,
-                        fill: true,
-                        backgroundColor: 'rgba(380, 200, 230, 0.2)',
-                        borderColor: 'rgb(380, 200, 230)',
-                        data: [30, 10, 27, 19, 33, 15, 19, 20, 24, 15, 37, 6]
+                    options: {
+                        plugins: {
+                        legend: {
+                            labels: {
+                            usePointStyle: true,
+                            },
+                        }
+                        }
                     }
+                });
+    
+        // Fetch data and update chart
+        let filter = 'monthly';
+        let time = 2024;
+        fetchDataAndUpdateChart(filter,time);
+    }
 
-                ]
-            },
-            options: {
-                plugins: {
-                legend: {
-                    labels: {
-                    usePointStyle: true,
-                    },
-                }
-                }
-            }
-        });
-    } //End if
+
+
+
+
+
 
     /*Sale statistics Chart*/
     if ($('#myChart2').length) {
